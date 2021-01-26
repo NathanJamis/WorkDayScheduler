6d9a55c6d9f63d24dc904947238f595f5a7b8a18
Nate J <jamis.walsh@gmail.com>
2021-01-25 21:08:10 -0800

    small changes

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,8 @@ var currentDay = document.getElementById("currentDay");
 
 currentDay.textContent = m.format('LLLL')
 
+// Set vars for time blocks
 
+// Set queries for time blocks --- to set hour block color
 
-
+// localStorage for textarea inputs
